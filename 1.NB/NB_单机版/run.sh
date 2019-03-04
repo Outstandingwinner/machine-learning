68ee6b3b778bfb4c1ac01f335ed269875ed25184
yajun.mao <yajun.mao@yoorstore.com>
2019-03-04 14:48:06 +0800

    update

--- 1.NB/NB_单机版/run.sh
+++
@@ -1,7 +0,0 @@
-python DataConvert.py data/ nb_data
-
-# train
-python NB.py 1 nb_data.train model
-
-# test
-python NB.py 0 nb_data.test model out
--- NER/NERuselocal/servers/checkdata.py
+++
@@ -1,3 +0,0 @@
-lines=open('D:\\code\\python\\cner\\bk\\NER_IDCNN_CRF\\NER_IDCNN_CRF\\data\\example.dev','r',encoding='utf8').readlines()
-for line in lines:
-    print(line)
